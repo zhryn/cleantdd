df52b73c8f910ae28dac7a3c2051fee9d7e11e8a
Muhammad Zuhriyan Sauqi <hello@zuhriyansauqi.com>
2022-02-03 05:21:48 +0700

    Refactored imports

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,8 +1,9 @@
-import 'package:cleantdd/core/error/failures.dart';
-import 'package:cleantdd/core/usecases/usecase.dart';
-import 'package:cleantdd/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:cleantdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
